8bdd012ac4b13335e88890d0aa8de9944581ec12
Tiji <143082629+IamTiji@users.noreply.github.com>
2023-12-14 21:00:00 +0800

    I am lazy to add this one

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+  
+setup( 
+    name='mordenizedtk', 
+    version='rc1.0', 
+    description='Get more mordenized gui', 
+    author='Tiji', 
+    author_email='abctiji@gmail.com', 
+    packages=['mordenizedtk'], 
+    install_requires=[ 
+        'pillow',  
+    ], 
+) 
