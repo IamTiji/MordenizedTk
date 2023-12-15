f1696ee6248996a894e08b4a71c455c773e0cd64
Tiji <143082629+IamTiji@users.noreply.github.com>
2023-12-15 17:03:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
   
 setup( 
     name='mordenizedtk', 
-    version='1.0rc1', 
+    version='1.0rc2', 
     description='Get more mordenized gui', 
     author='Tiji', 
     author_email='abctiji@gmail.com', 
